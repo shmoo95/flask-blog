4175c12ac989c2f5951606b67a3db5a8fee500be
Joseph C. Burwasser <josephcburwasser@gmail.com>
2014-10-08 19:53:27 -0600

    Web commit1

--- blog.py
+++ blog.py
@@ -7,6 +7,13 @@ import sqlite3
 
 #configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
+
+
+
+
 
 app=Flask(__name__)
 
@@ -17,14 +24,26 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Credentials. Please try again'
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('main'))
+	return render_template('login.html', error=error)
 
 @app.route('/main')
 def main():
 	return render_template('main.html')
 
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
